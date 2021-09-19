0fab71a7a4517a7804c95c2c454c3c8e8d7c2e54
Rameshwar Sah <rameshpfcl7@gmail.com>
2021-09-19 15:26:44 -0500

    this is even odd condition checking coding

---
+++ ifelse/src/Welcome.java
@@ -0,0 +1,19 @@
+public class Welcome {
+    public static void main(String[] args) {
+
+
+        int n1 = 10;
+        if (n1 > 0) {
+            if(n1%2==0){
+                System.out.println("even number");
+            }
+            else{
+                System.out.println("odd number");
+            }
+
+        }
+        else{
+            System.out.println("plz enter positive number");
+        }
+    }
+}
