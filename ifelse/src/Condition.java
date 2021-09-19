57637df344face69be59e285d687d9c72da751f7
Rameshwar Sah <rameshpfcl7@gmail.com>
2021-09-19 15:21:25 -0500

    this is ifelseif condition checking coding

---
+++ ifelse/src/Condition.java
@@ -0,0 +1,13 @@
+public class Condition {
+    public static void main(String[] args) {
+        int n1 = 0;
+        if (n1>0){
+            System.out.println("positive number");
+        }
+        else if(n1<0){
+            System.out.println("negative number");
+        }
+        else System.out.println("zero number");
+
+    }
+}
