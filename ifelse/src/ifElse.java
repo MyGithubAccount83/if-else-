e47fae68c7ea152c9431c6c67e9afd5cc7c93bb7
Rameshwar Sah <rameshpfcl7@gmail.com>
2021-09-19 15:13:49 -0500

    this is ifelse condition checking coding

---
+++ ifelse/src/ifElse.java
@@ -0,0 +1,12 @@
+public class ifElse {
+    public static void main(String[] args) {
+
+
+        int n = 15;
+        if (n >= 18) {
+            System.out.println("eligible for voting");
+        } else {
+            System.out.println("not eligible for voting");
+        }
+    }
+}
